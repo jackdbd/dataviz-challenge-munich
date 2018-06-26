16e50e63a1a9ca495979e50755a2cc9df4de47be
jackdbd <jackdebidda@gmail.com>
2018-06-26 12:47:38 +0200

    Add data loading in componentDidMount

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,40 @@ const Content = styled.div`
   flex-basis: auto;
 `;
 
+const rowFunction = d => {
+  /*
+    There is no relationship between the number of books of a given genre, and
+    the number of books of other genres (e.g. a single person could buy 1 Satire
+    book and: 10 Science Fiction books + 2 Drama books, etc...).
+  */
+  const genre = d[""];
+  const obj = {
+    genre
+  };
+  const entries = Object.entries(d).filter(e => e[0] !== "");
+  entries.forEach(e => {
+    const otherGenre = e[0];
+    const numBooks = +e[1];
+    obj[otherGenre] = numBooks;
+  });
+  return obj;
+};
+
 class App extends Component {
+  componentDidMount() {
+    console.log("componentDidMount");
+    const urlDataset = "../data/book_genres.tsv";
+    // const urlDataset = "https://github.com/jackdbd/d3-visualizations/blob/master/src/data/book_genres.tsv";
+    // const urlDataset = "https://s3.eu-central-1.amazonaws.com/dataviz-challenge-munich-giacomo-debidda/data/book_genres.tsv";
+    const promise = d3.tsv(urlDataset, rowFunction);
+    promise
+      .then(dataset => {
+        console.log(dataset);
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
   render() {
     return (
       <Container>
