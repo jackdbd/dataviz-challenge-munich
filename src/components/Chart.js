57f5ec759739c8e1b883bdb460b9ca1dbd5414c7
jackdbd <jackdebidda@gmail.com>
2018-06-30 19:25:51 +0200

    Move DebugSVG component to its own file

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,84 +4,12 @@ import { format as d3Format } from "d3-format";
 import { withParentSize } from "@vx/responsive";
 import { Bar } from "@vx/shape";
 import { AxisLeft, AxisBottom } from "@vx/axis";
-import { GradientPinkBlue } from "@vx/gradient";
-import { Text } from "@vx/text";
 import D3MarginConvention from "./D3MarginConvention";
 import Header from "./Header";
 import ChartFooter from "./ChartFooter";
+import DebugSVG from "./DebugSVG";
 
-// TODO: add withDebugSVG, a HOC that wraps a SVG component
-// Maybe it would be more appropriate to call it DebugSVGViewBox
-function DebugSVG(props) {
-  const { width, height, viewBox, margin } = props;
-  const outerWidth = width;
-  const outerHeight = height;
-  const innerWidth = outerWidth - margin.left - margin.right;
-  const innerHeight = outerHeight - margin.top - margin.bottom;
-  const center = {
-    x: margin.left + innerWidth / 2,
-    y: margin.top + innerHeight / 2
-  };
-  const r = 5;
-  return (
-    <React.Fragment>
-      <GradientPinkBlue id="debug-svg-gradient" />
-      <g className={"debug-svg"}>
-        <rect
-          x={0}
-          y={0}
-          width={outerWidth}
-          height={outerHeight}
-          style={{ fill: "url(#debug-svg-gradient)", opacity: 0.7 }}
-        />
-        <Text
-          x={0}
-          y={0}
-          dx={5}
-          dy={5}
-          verticalAnchor="start"
-        >{`${outerWidth} x ${outerHeight}`}</Text>
-      </g>
-      <g className={"debug-svg-margins"}>
-        <rect
-          x={margin.left}
-          y={margin.top}
-          width={innerWidth}
-          height={innerHeight}
-          style={{
-            fill: "none",
-            stroke: "black",
-            strokeWidth: 2,
-            strokeDasharray: "4 1"
-          }}
-        />
-        <Text
-          x={margin.left}
-          y={margin.top}
-          dx={5}
-          dy={5}
-          verticalAnchor="start"
-        >{`${innerWidth} x ${innerHeight}`}</Text>
-      </g>
-      <g className="svg-debug-center">
-        <circle cx={center.x} cy={center.y} r={r} style={{ fill: "red" }} />
-        <Text x={center.x + r} y={center.y} verticalAnchor="middle">{`(${
-          center.x
-        }, ${center.y})`}</Text>
-      </g>
-      <g className={"debug-svg-viewbox"}>
-        <Text
-          x={margin.left + innerWidth}
-          y={margin.top + innerHeight}
-          dx={-5}
-          dy={-5}
-          textAnchor="end"
-          verticalAnchor="end"
-        >{`Viewbox: ${viewBox}`}</Text>
-      </g>
-    </React.Fragment>
-  );
-}
+// TODO: reuse code between Chart and ComparisonChart. With a HOC maybe?
 
 function Chart(props) {
   const {
